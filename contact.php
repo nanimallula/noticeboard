47dfc2d9586158fdf5f1fe5605a41f3d7e6aa5f2
Venkata sreeram <48221427+nanimallula@users.noreply.github.com>
2020-01-11 20:28:47 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,50 @@
+<h2>Quick Contact</h2>
+<form method="post">
+	<div class="row">
+		<div class="col-md-8">
+		<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	
+	
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Email</div>
+		<div class="col-sm-5">
+		<input type="email" name="e" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Mobile</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4">Enter YOur Message</div>
+		<div class="col-sm-5">
+		<input type="password" name="p" class="form-control"/></div>
+	</div>
+	
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+		<input type="submit" value="Send Query" name="save" class="btn btn-success"/>
+		
+		</div>
+	</div>
+		
+		</div>
+		<div class="col-md-4">
+		<h2>Contact us</h2>
+		
+		Name: Venkata Sreeram Mallula<br/>
+		Mobile: 8317563303<br/>
+		Email:venkata.sreeram11087@gmail.com<br/>
+		Website: <a href="https://tap.ibhubs.in/2019/sreeram-mallula/sreeramportfolio.html">VENKATA MALLULA</a>
+		</div>
+	</div>
+	
+	
+</form>	
